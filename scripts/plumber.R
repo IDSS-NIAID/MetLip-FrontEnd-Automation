387f4f4a612f5807122482e9792583be147a5d14
lightning-puma <lacroixis@nih.gov>
2025-04-08 16:14:28 -0600

    Create plumber.R

---
+++ scripts/plumber.R
@@ -0,0 +1,8 @@
+library(plumber)
+library(jose)
+
+# Shared secret — must match the one in your Shiny app
+my_secret <- charToRaw("my-123-bit-secret")
+
+# Path to the Excel file to serve
+file_path <- "TAS_lacroixis_20220420_ML1_Submitted_Samples.xlsx"
