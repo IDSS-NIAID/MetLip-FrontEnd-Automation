16eea6ce6fcc9289c683a428287fc10a709dbb36
lightning-puma <lacroixis@nih.gov>
2025-04-08 16:15:07 -0600

    Update plumber.R
    Add a health check

--- scripts/plumber.R
+++ scripts/plumber.R
@@ -6,3 +6,9 @@ my_secret <- charToRaw("my-123-bit-secret")
 
 # Path to the Excel file to serve
 file_path <- "TAS_lacroixis_20220420_ML1_Submitted_Samples.xlsx"
+
+#* Health check
+#* @get /health
+function() {
+  list(status = "OK", time = Sys.time())
+}
