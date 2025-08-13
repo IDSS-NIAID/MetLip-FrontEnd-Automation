8e1ae70fe671673d8c5acb3a95cd8bcfebe87a56
lightning-puma <lacroixis@nih.gov>
2025-08-13 15:58:22 -0600

    Update process_acquisition_ids_reduced.R

--- R/process_acquisition_ids_reduced.R
+++ R/process_acquisition_ids_reduced.R
@@ -1,6 +1,7 @@
-#' Process sample submission data and generate acquisition IDs
+#' Process sample acquisition IDs in preparation for plate meta data generation
 #'
-#' This function processes TAS sample submission data by generating unique acquisition IDs
+#' This function was largely reduced from the original.
+#' Will be maintained in case the acquisition ID generation strategy is continued.
 #' based on the file name structure and selected mass spectrometry (MS) methods.
 #'
 #' @param submitted_sample_data A data frame containing acquisition IDs, with column `Submitted Sample Ids`.
@@ -8,10 +9,9 @@
 #' @return A data frame with expanded sample submission data, including generated acquisition IDs.
 #' @export
 #'
-#' @importFrom dplyr intersect slice mutate n group_by ungroup
+#' @importFrom dplyr mutate n group_by ungroup
 #' @importFrom stringr str_split
 #' @importFrom lubridate year
-#' @importFrom writexl write_xlsx
 process_acquisition_ids <- function(submitted_sample_data, selected_ms_methods) {
   # take care of annoying no visible binding note
   if(FALSE)
@@ -35,33 +35,13 @@ process_acquisition_ids <- function(submitted_sample_data, selected_ms_methods) 
   acq_prefix <- paste0("MLA", current_year, "_", request_id) #sneaky paste to ensure the year is attached to MLA
   
   
-  # Filter categories based on user selection
+  # Define the MS methods used in this project
   ms_methods <- unique(submitted_sample_data$MS_method)
   
-  # Expand the sample submission data to repeat each submitted name based on selected ms methods
-  submitted_sample_data_expanded <- submitted_sample_data |>
-    slice(rep(1:n(), each = length(ms_methods))) |>
-    mutate(MS_method = rep(ms_methods, times = n() / length(ms_methods)),
-           Date_Processed = Sys.Date())
-  
-  # Generate new unique Acquisition IDs by appending _0001 through the appropriate count
-  # This data frame will be passed along to the next step in the MetLip Dashboard (generate plate data)
-  isl_keep <- submitted_sample_data_expanded |>
-    mutate(Project_ID = parsed_proj_id,
-           Acquired_Sample_ID = paste(acq_prefix, sprintf("%05d", row_number()), sep = "-"), 
-           Acquired_Sample_Name = paste(`Submitted Sample Names`, MS_method, sep = "_")) |> 
-    relocate(c(Acquired_Sample_ID, Acquired_Sample_Name), .before = `Submitted Sample Ids`)
-  
-  # Remove un-necessary columns
-  # This data frame will be saved to the working directory. Then, uploaded to TAS.
-  isl_out <- isl_keep |> 
-    select(-c(MS_method, Project_ID))
-  
-  # File name used for isl_out  
-  acq_file_name <- gsub("Submitted_Samples", "Acquired_Samples", file_name_raw)
-  
-  # Save the excel file
-  writexl::write_xlsx(x = isl_out, path = acq_file_name)
+ 
+  # Generate project ID for downstream functions
+  isl_keep <- submitted_sample_data |>
+    mutate(Project_ID = parsed_proj_id)
   
   return(isl_keep)
 }
